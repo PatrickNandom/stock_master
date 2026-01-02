de68724418e00e989d3f9d88922f601cbea2ea3d
Nandom Patrick Molshakat <139545102+PatrickNandom@users.noreply.github.com>
2026-01-02 18:39:13 +0100

    refactor: reorganize project file structure

--- app/components/CustomInput .tsx
+++ app/components/CustomInput .tsx
@@ -13,12 +13,14 @@ const CustomInput = React.forwardRef<HTMLInputElement, CustomInputProps>(
         <label className="flex justify-center mb-1 font-medium text-gray-700">
           {label}
         </label>
+
         <input
           ref={ref}
           className={`w-full px-2 border border-orange-500 rounded-[5]  bg-white  text-lg focus:outline-none
             ${error ? "border-red-500" : ""}`}
           {...props}
         />
+
         {error && <p className="text-red-600 text-sm mt-1">{error}</p>}
       </div>
     );
