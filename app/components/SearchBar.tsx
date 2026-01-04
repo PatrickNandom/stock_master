c4bb1d7c67db7ed8339a6ee7a906028b338582f3
Nandom Patrick Molshakat <139545102+PatrickNandom@users.noreply.github.com>
2026-01-05 00:44:00 +0100

    feat: implement topbar compnent

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import { ChangeEvent } from "react";
+
+type SearchBarProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
+  return (
+    <div className="relative w-30 h-8">
+      <Image
+        src="/dashboard_topbar_search_icon.svg"
+        alt="Search"
+        width={11}
+        height={11}
+        className="absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none"
+      />
+
+      <input
+        type="text"
+        value={value}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          onChange(e.target.value)
+        }
+        className="w-full h-full rounded-3xl bg-white text-black py-2 pl-10 pr-3 outline-none"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -1,6 +1,46 @@
+"use client";
+import SearchBar from "./SearchBar";
+import Image from "next/image";
+
 const Topbar = () => {
   return (
-    <header className="h-16 bg-linear-to-r from-[#F7AB97] to-[#071548] flex items-center justify-between px-6 text-white"></header>
+    <header className="h-16 bg-linear-to-r from-[#F7AB97] to-[#071548] flex items-center justify-between px-6 text-white">
+      <div className="flex items-center gap-4">
+        <SearchBar value="" onChange={() => {}} />
+
+        <div className="flex justify-center items-center w-8 h-8 bg-white rounded-4xl p-1">
+          <Image
+            src="/dashboard_topbar_notification_icon.svg"
+            alt="notification"
+            width={11}
+            height={11}
+          />
+        </div>
+      </div>
+
+      <h1 className="text-xl text-white">Dashoard</h1>
+
+      <div className="flex gap-4">
+        <div className="flex justify-center w-8 h-8 items-center bg-white rounded-4xl p-1">
+          <Image
+            src="/dashboard_topbar_settings_icon.svg"
+            alt="notification"
+            width={11}
+            height={11}
+          />
+        </div>
+
+        <div className="w-[130] h-8  bg-white rounded-2xl px-2 flex justify-around items-center">
+          <Image
+            src="/dashboard_topbar_user_icon.svg"
+            alt="user"
+            width={11}
+            height={11}
+          />
+          <p className="text-black">User</p>
+        </div>
+      </div>
+    </header>
   );
 };
 export default Topbar;
