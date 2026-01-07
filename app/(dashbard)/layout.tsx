f6991954d06930d5edac0bd70b6fc619924e45cc
Nandom Patrick Molshakat <139545102+PatrickNandom@users.noreply.github.com>
2026-01-07 21:15:22 +0100

    feat: add latest landscape responsive

--- app/(dashbard)/layout.tsx
+++ app/(dashbard)/layout.tsx
@@ -5,7 +5,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
-        <div className="flex min-h-screen bg-gray-100">
+        <div className="flex max-h-screen bg-gray-100">
           <Sidebar />
           <div className="flex-1 flex flex-col min-w-0">
             <Topbar />
