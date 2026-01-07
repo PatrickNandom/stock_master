a72f1de259074256f1fd765d6e5731a1d53bcd0a
Nandom Patrick Molshakat <139545102+PatrickNandom@users.noreply.github.com>
2026-01-07 20:50:34 +0100

    feat: add landscape responsive

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -47,7 +47,7 @@ const Sidebar = () => {
   const pathname = usePathname();
   return (
     <aside
-      className="max-w-50  hidden bg-[#CDD0DA] sm:block border-r border-gray-300 sticky top-0 sm:h-screen sh-dvh landscape:max-h-[500px]:w-20s pl-[env(safe-area-inset-left)] "
+      className="max-w-50  hidden bg-[#CDD0DA] sm:block border-r border-gray-300 sticky top-0 sm:h-screen h-dvh  pl-[env(safe-area-inset-left)] "
     >
       <div className="mb-8 h-16 bg-[#F7AB97] font-bold text-xl flex items-center justify-around">
         <Link href="/">
