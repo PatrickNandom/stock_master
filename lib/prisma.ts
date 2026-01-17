8b0be272f6b820eb0dd1f09f752eb6f4ae909ed2
Nandom Patrick Molshakat <139545102+PatrickNandom@users.noreply.github.com>
2026-01-17 03:33:41 +0100

    fix: dp migration error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,32 @@
 // app/lib/prisma.ts
-import { PrismaClient } from "@/app/generated/prisma/client";
+// import { PrismaClient } from "@/app/generated/prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log:
+//       process.env.NODE_ENV === "development"
+//         ? ["query", "error", "warn"]
+//         : ["error"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
+
+// export default prisma;
+// lib/prisma.ts (or app/lib/prisma.ts)
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
